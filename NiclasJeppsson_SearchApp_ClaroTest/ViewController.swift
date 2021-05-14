28efcff039f81671d31db438bf15cbb8dda74f0a
Niclas Jeppsson <niclasjeppsson@gmail.com>
2021-05-14 11:29:50 +0100

    Initial Commit

---
+++ NiclasJeppsson_SearchApp_ClaroTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NiclasJeppsson_SearchApp_ClaroTest
+//
+//  Created by Niclas Jeppsson on 14/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
